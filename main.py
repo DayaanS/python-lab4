06c6e16a5acb080fbc7b40828281046f4dff45f6
Dayaana <daiaanasemyonova@yandex.ru>
2025-11-26 13:09:57 +0300

    find selected

--- main.py
+++ main.py
@@ -1,53 +1,125 @@
-items = {
-    'r': {'size': 3, 'points': 25},
-    'p': {'size': 2, 'points': 15},
-    'a': {'size': 2, 'points': 15},
-    'm': {'size': 2, 'points': 20},
-    'i': {'size': 1, 'points': 5},
-    'k': {'size': 1, 'points': 15},
-    'x': {'size': 3, 'points': 20},
-    't': {'size': 1, 'points': 25},
-    'f': {'size': 1, 'points': 15},
-    'd': {'size': 1, 'points': 10},
-    's': {'size': 2, 'points': 20},
-    'c': {'size': 2, 'points': 20}
+items_dict = {
+    'r': {'slots': 3, 'points': 25},
+    'p': {'slots': 2, 'points': 15},
+    'a': {'slots': 2, 'points': 15},
+    'm': {'slots': 2, 'points': 20},
+    'i': {'slots': 1, 'points': 5},
+    'k': {'slots': 1, 'points': 15},
+    'x': {'slots': 3, 'points': 20},
+    't': {'slots': 1, 'points': 25},
+    'f': {'slots': 1, 'points': 15},
+    'd': {'slots': 1, 'points': 10},
+    's': {'slots': 2, 'points': 20},
+    'c': {'slots': 2, 'points': 20}
 }
 
-must_have = items['d']
-items.pop('d')
-max_size = 9 - must_have['size']
-def gen_table(items, max_size = max_size):
-    table = [[0 for _ in range(max_size)] for _ in range (len(items))]
-    for i, item in enumerate(items.values()):
-        size = item['size']
-        points = item['points']
-
-        for limit_size in range(1, max_size + 1):
-            idx = limit_size - 1
-            if i == 0:
-                if size > limit_size:
-                    table[i][idx] = 0  
-                else:
-                    table[i][idx] = points
-            else: 
-                prev_points = table[i-1][idx]
-                if size > limit_size:
-                    table[i][idx] = prev_points
-                else:
-                    if idx < size:
-                        rest = 0
-                    else:
-                        table[i-1][idx - size]
-                    result = max(rest + points, prev_points)
-                    table[i][idx] = result
-    for i in table: print(i)
+inv_slots_x = 3
+inv_slots_y = 3
+inv_slots = inv_slots_x * inv_slots_y
+# заражение - антидот обязателен
+# убираем антидот из списка чтобы не положить в рюкзак дубликат
+must_have = items_dict.pop('d')
+max_slots = inv_slots - must_have['slots']
+start_points = 15
+chr_points = start_points # + must_have['points']
 
 
-def get_size_and_points(items):
-    size = [items[item]['size'] for item in items]
-    points = [items[item]['points'] for item in items]
-    return size, points
-size, points = get_size_and_points(items)
-print(size)
-print(points)
+def get_slots_and_points(items_dict):
+    '''
+    Создаём списки значений площади и ценности
+    '''
+    slots = [items_dict[item]['slots'] for item in items_dict]
+    points = [items_dict[item]['points'] for item in items_dict]
+    return slots, points
 
+
+def get_table(items_dict, max_slots):
+    n = len(items_dict) 
+    slots, points = get_slots_and_points(items_dict)
+
+    # zeroes table max slots + 1 colums and items_dict count rows rows
+    table = [[0 for _ in range(max_slots+1)] for _ in range(n+1)]
+
+    
+    for row in range(n+1):
+        for col in range (max_slots+1):
+            prev_points = table[row-1][col]
+            # can just remove this zeroes colums and row? seems to just add confusion with indexes
+            if row == 0 or col==0:
+                table[row][col] = 0
+            # если площадь предмета меньше площади столбца, 
+            # максимизируем значение суммарной ценности
+            elif slots[row-1] <= col: # -1 row is item. - 1 cause 1 row i zeroes
+                current_points = points[row-1] + table[row-1][col-slots[row-1]]
+                table[row][col] = max(current_points, prev_points)
+            # если площадь предмета больше площади столбца,
+            # забираем значение ячейки из предыдущей строки
+            else: # оазмео больше макс размера            
+                table[row][col] = prev_points
+    return table, slots, points
+
+
+def get_selected_items(items_dict, max_slots):
+    table, slots, points = get_table(items_dict, max_slots)
+    n = len(items_dict)
+    res = table[n][max_slots] # last element [8][11]
+    print(res)
+    a = max_slots
+    items_list = []
+
+    for i in range(n, 0, -1):
+        if res <= 0: 
+            break
+        if res == table[i-1][a]:
+            continue
+        else:
+            items_list.append({'slots': slots[i-1], 'points':points[i-1]})
+            res -= points[i-1]
+            a -= slots[i-1]
+    print(items_list)
+
+    selected_items = []
+    for search in items_list:
+        selected_item = []
+        for key, value in items_dict.items():
+            if value == search:
+                selected_item.append(key)
+        selected_items.append(selected_item)
+    return selected_items
+
+    # inv_inventory_variants = []
+    # inv_inv = [] 
+    # for variant in selected_items:  
+    #     inv_slot = '' 
+    #     #for j in range(len(i)):
+    #         #if j not in inv_inv:
+    #             #inv_inv += i[j]
+    #     inv_inv += inv_slot]
+    # print(inv_inv)
+
+
+get_selected_items(items_dict, max_slots)
+inv_variant = ['k', 't', 'f', 'm', 'r', 'd']
+
+def calc_chr_points(items_dict, chr_points):
+    for item in items_dict:
+        if item in inv_variant:
+            chr_points += items_dict[item]['points']
+        else:
+            chr_points -= items_dict[item]['points']
+    return chr_points
+
+print(calc_chr_points(items_dict, chr_points))
+# 'k': {'slots': 1, 'points': 15}
+# 't': {'slots': 1, 'points': 25}
+# 'f': {'slots': 1, 'points': 15}
+# 'm': {'slots': 2, 'points': 20}
+# 'r': {'slots': 3, 'points': 25}
+# 'd': {'slots': 1, 'points': 10}
+
+inv = [[0 for _ in range(inv_slots_x)] for _ in range(inv_slots_y)]
+
+print(inv)
+
+
+#[['k', 'f'], ['t'], ['k', 'f'], ['m', 's', 'c'], ['r']]
